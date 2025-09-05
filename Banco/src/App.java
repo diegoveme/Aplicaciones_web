9617597b834b78fe28b247a1d4f2a4677fa38e96
diegoveme <diegosanti2006@hotmail.com>
2025-09-04 22:18:20 -0600

    feat: create main App class and user input collection

---
+++ Banco/src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter client name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter account number: ");
+        String account = scanner.nextLine();
+
+        double balance = 0.0;
+        boolean running = true;
+
+    }
+}
