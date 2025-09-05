ed5d764448ee4384e47afa3f050ab780c1293a13
diegoveme <diegosanti2006@hotmail.com>
2025-09-04 22:01:19 -0600

    feat: implement deposit execution logic

--- Banco/src/Depositar.java
+++ Banco/src/Depositar.java
@@ -4,5 +4,14 @@ public class Depositar {
     public Depositar(double amount) {
         this.amount = amount;
     }
-
+    public double execute(double currentBalance) {
+        if (amount > 0) {
+            double newBalance = currentBalance + amount;
+            System.out.println("Deposit successful. Total balance: $" + newBalance);
+            return newBalance;
+        } else {
+            System.out.println("Invalid deposit amount.");
+            return currentBalance;
+        }
+    }
 }
