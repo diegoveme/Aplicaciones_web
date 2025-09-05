cca3771d97ddd7934fc2729b7e74ff9ced4b8e2f
diegoveme <diegosanti2006@hotmail.com>
2025-09-04 22:20:21 -0600

    feat: implement main menu and switch statement

--- Banco/src/App.java
+++ Banco/src/App.java
@@ -13,5 +13,19 @@ public class App {
         double balance = 0.0;
         boolean running = true;
 
+        while (running) {
+            System.out.println("\nChoose an option:");
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Consult");
+            System.out.println("4. Exit");
+
+            int option = scanner.nextInt();
+
+            switch (option) {
+                default:
+                    System.out.println("Invalid option.");
+            }
+        }
     }
 }
