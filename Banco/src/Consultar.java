7d3b4a4abdb1366a69b21ea286909cce9a766d73
diegoveme <diegosanti2006@hotmail.com>
2025-09-04 22:14:02 -0600

    feat: add private attributes and constructor

--- Banco/src/Consultar.java
+++ Banco/src/Consultar.java
@@ -1,3 +1,11 @@
 public class Consultar {
-    
+     private String clientName;
+    private String accountNumber;
+    private double balance;
+
+    public Consultar(String clientName, String accountNumber, double balance) {
+        this.clientName = clientName;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
 }
